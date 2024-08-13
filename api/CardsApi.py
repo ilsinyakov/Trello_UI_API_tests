993673150f541a8a13bf9515aed06c23b8254dbc
ilsinyakov <ilsinyakov@gmail.com>
2024-08-13 21:36:51 +0300

    test API

--- api/CardsApi.py
+++ api/CardsApi.py
@@ -8,7 +8,7 @@ class CardsApi:
         self.api_key = api_key
         self.token = token
 
-    @step("Create card by API")
+    @step('Create card by API')
     def create_card(self, list_id: str, card_name: str) -> dict:
         path = (
             f'{self.base_url}/cards/?idList={list_id}'
@@ -19,4 +19,62 @@ class CardsApi:
         }
 
         resp = requests.post(path, json=body)
+        return resp.json()
+
+    @step('Delete card by API')
+    def delete_card(self, card_id: str) -> int:
+        path = (
+            f'{self.base_url}/cards/{card_id}?'
+            f'&key={self.api_key}&token={self.token}'
+        )
+        resp = requests.delete(path)
+        return resp.status_code
+
+    @step('Get card info by API')
+    def get_card(self, card_id: str) -> dict:
+        path = (
+            f'{self.base_url}/cards/{card_id}?'
+            f'&key={self.api_key}&token={self.token}'
+        )
+        resp = requests.get(path)
+        return resp.json()
+
+    def is_card_list_updated(self, card_id: str, new_list_id: str) -> bool:
+        list_id = self.get_card(card_id)["idList"]
+        if list_id == new_list_id:
+            return True
+        else:
+            return False
+
+    def is_card_name_updated(self, card_id: str, new_card_name: str) -> bool:
+        card_name = self.get_card(card_id)["name"]
+        if card_name == new_card_name:
+            return True
+        else:
+            return False
+
+    @step('Update card name by API')
+    def update_card_name(self, card_id: str, new_card_name: str) -> dict:
+        path = (
+            f'{self.base_url}/cards/{card_id}?'
+            f'&key={self.api_key}&token={self.token}'
+        )
+        body = {
+            "name": new_card_name
+        }
+
+        resp = requests.put(path, json=body)
+        return resp.json()
+
+    @step('Update card column by API')
+    def update_card_list(self, card_id: str, new_list_id: str) -> dict:
+        path = (
+            f'{self.base_url}/cards/{card_id}?'
+            f'&key={self.api_key}&token={self.token}'
+        )
+        body = {
+            "idList": new_list_id
+        }
+
+        resp = requests.put(path, json=body)
         return resp.json()
--- test/test_api.py
+++ test/test_api.py
@@ -47,3 +47,77 @@ def test_create_card(board_api_client, card_api_client, list_api_client):
     with step('Check that created card is in the list'):
         assert list_api_client.is_card_in_list(list_id, card_id), \
             'Created card is not in the list'
+
+    status_code = board_api_client.delete_board_by_name(board_name)
+    with step('Check that the created board has been deleted'):
+        assert status_code == 200, 'The created board has not been deleted'
+
+
+@allure.feature('Card')
+@allure.severity(severity_level.CRITICAL)
+@allure.title('Test update card by API')
+def test_update_card(board_api_client, card_api_client):
+    board_name = 'Board to update card'
+    card_name = 'Test card 6666'
+    new_card_name = 'Updated card name'
+    board_id = board_api_client.create_board(board_name)["id"]
+    list_id = board_api_client.get_lists_on_board(board_id)[0]["id"]
+    card_id = card_api_client.create_card(list_id, card_name)["id"]
+
+    card_api_client.update_card_name(card_id, new_card_name)
+
+    with step('Check updated card name'):
+        assert card_api_client.is_card_name_updated(card_id, new_card_name), \
+            'Card name has not been updated'
+
+    status_code = board_api_client.delete_board_by_name(board_name)
+    with step('Check that the created board has been deleted'):
+        assert status_code == 200, 'The created board has not been deleted'
+
+
+@allure.feature('Card')
+@allure.severity(severity_level.CRITICAL)
+@allure.title('Test delete card by API')
+def test_delete_card(board_api_client, card_api_client, list_api_client):
+    board_name = 'Board to delete card'
+    card_name = 'Test card to delete'
+
+    board_id = board_api_client.create_board(board_name)["id"]
+    list_id = board_api_client.get_lists_on_board(board_id)[0]["id"]
+    card_id = card_api_client.create_card(list_id, card_name)["id"]
+
+    status_code = card_api_client.delete_card(card_id)
+    with step('Check that the card has been deleted'):
+        assert status_code == 200, 'The card has not been deleted'
+        assert not list_api_client.is_card_in_list(list_id, card_id), \
+            "There is deleted card in the list"
+
+    status_code = board_api_client.delete_board_by_name(board_name)
+    with step('Check that the created board has been deleted'):
+        assert status_code == 200, 'The created board has not been deleted'
+
+
+@allure.feature('Card')
+@allure.severity(severity_level.CRITICAL)
+@allure.title('Test move card by API')
+def test_move_card(board_api_client, card_api_client):
+    board_name = 'Board to move card'
+    card_name = 'Test card to move'
+    old_list_num = 0  # number of list to which we will create the card
+    new_list_num = 1  # number of list to which we will move the card
+
+    board_id = board_api_client.create_board(board_name)["id"]
+    list_id = board_api_client.get_lists_on_board(board_id)[old_list_num]["id"]
+    new_list_id = board_api_client.\
+        get_lists_on_board(board_id)[new_list_num]["id"]
+    card_id = card_api_client.create_card(list_id, card_name)["id"]
+
+    card_api_client.update_card_list(card_id, new_list_id)
+
+    with step('Check updated card column'):
+        assert card_api_client.is_card_list_updated(card_id, new_list_id), \
+            'Card column has not been updated'
+
+    status_code = board_api_client.delete_board_by_name(board_name)
+    with step('Check that the created board has been deleted'):
+        assert status_code == 200, 'The created board has not been deleted'
